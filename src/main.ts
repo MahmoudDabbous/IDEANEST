a27d4c83ef039a65da7227ef6a029811290179a9
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-10-29 07:47:16 +0300

    feat(documentaion): added swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Organization API')
+    .setVersion('1.0')
+    .addBearerAuth({ bearerFormat: 'JWT', type: 'http' })
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
